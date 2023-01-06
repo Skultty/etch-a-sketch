43d91dba57791b52c87761dd5d2339138b0ed0a7
Samuel <samuel1skultety@gmail.com>
2023-01-06 12:35:24 +0100

    feat: color on hover

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+const grid = document.querySelector('.grid');
+
 for (let i = 0; i < 256; i++) {
     let div = document.createElement('div');
     document.querySelector('.grid').appendChild(div);
 }
+
+grid.addEventListener('mouseover', (e) => {
+  if (e.target.tagName === 'DIV') {
+    e.target.style.backgroundColor = 'black';
+  }
+});

142832a8900a136e3c7437742054daadd98011fd
Samuel <samuel1skultety@gmail.com>
2023-01-06 12:47:12 +0100

    feat: added a button that creates a new blank grid

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 const grid = document.querySelector('.grid');
+const button = document.querySelector('.button');
 
-for (let i = 0; i < 256; i++) {
+function createGrid(size) {
+  for (let i = 0; i < size * size; i++) {
     let div = document.createElement('div');
-    document.querySelector('.grid').appendChild(div);
+    grid.appendChild(div);
+  }
+  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 }
+
+createGrid(16);
 
 grid.addEventListener('mouseover', (e) => {
   if (e.target.tagName === 'DIV') {
     e.target.style.backgroundColor = 'black';
   }
+});
+
+button.addEventListener('click', () => {
+  let size = prompt('Enter the number of squares per side for the new grid:');
+  size = parseInt(size);
+  while (size > 100) {
+    size = prompt('The number of squares per side should be 100 or less. Enter a new value:');
+    size = parseInt(size);
+  }
+  while (grid.firstChild) {
+    grid.removeChild(grid.firstChild);
+  }
+  createGrid(size);
 });
